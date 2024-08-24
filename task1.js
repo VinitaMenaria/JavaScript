4170f93f6f24d3fd26185ba22c6a598130c9f378
vinita <vinitamenaria2004@gmail.com>
2024-08-24 09:29:21 +0530

    task1

--- task1.js
+++ task1.js
@@ -13,14 +13,3 @@ else{
 }
 
 
-
-let age=prompt("Enter your age:");
-if(age>18){
-    console.log("You are eligible");
-}
-else if(age<18){
-    console.log("You are not eligible");
-}
-else{
-    console.log("Invalid input");
-}
